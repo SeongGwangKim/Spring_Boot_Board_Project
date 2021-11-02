7607a23c9d349513dacb5645ba19446a27583783
SeongGwangKim <78336335+SeongGwangKim@users.noreply.github.com>
2021-11-02 20:17:24 +0900

    첫 환경 설정

---
+++ SupplementsWebProject/src/main/java/com/Board/SupplementsWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplementsWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplementsWebProjectApplication.class, args);
+	}
+
+}
